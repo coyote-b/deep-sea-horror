6a0206b80f7878b2faa99a9984080342d4b4a910
lbunting-admin <lbunting@conestogac.on.ca>
2024-05-12 21:17:00 -0400

    Added the oxygen boost effect to movement

--- Assets/Scripts/Diver.cs
+++ Assets/Scripts/Diver.cs
@@ -1,15 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/// <summary>
+/// A component for the player character, the diver.
+/// </summary>
 public class Diver : MonoBehaviour
 {
     [SerializeField]
     SwimMovement _movement;
 
+    /// <summary>
+    /// Pushes the player in the direction pulled from the InputAction context.
+    /// </summary>
+    /// <param name="context">The InputAction context containing the direction the user pressed.</param>
     public void Swim(InputAction.CallbackContext context)
     {
         _movement.Swim(context.ReadValue<Vector2>()); 
+    }
+
+    /// <summary>
+    /// Pushes the player with faster speed, for a quick boost.
+    /// </summary>
+    public void OxygenBoost()
+    {
+        _movement.Boost();
     }
 }
--- Assets/Scripts/Movement/SwimMovement.cs
+++ Assets/Scripts/Movement/SwimMovement.cs
@@ -1,19 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
+/// <summary>
+/// A component for underwater movement.
+/// </summary>
 public class SwimMovement : MonoBehaviour
 {
-    [SerializeField]
-    float _speed;
+    private Vector2 _lastAppliedForce = new Vector2(0, 1);
 
     [SerializeField]
     Rigidbody2D _body;
 
+    [SerializeField]
+    float _movementSpeed;
+
+    [SerializeField]
+    float _boostSpeed;
+
+    /// <summary>
+    /// Applies force to the RigidBody according to the force specified.
+    /// </summary>
+    /// <param name="force">The direction of force.</param>
     public void Swim(Vector2 force)
     {
-        Debug.Log(force);
-        _body.AddForce(new Vector2(force.x * _speed, force.y * _speed));
+        Move(force, _movementSpeed);
+    }
+
+    /// <summary>
+    /// Applies force to the RigidBody according to the boost speed and the last applied force.
+    /// </summary>
+    public void Boost()
+    {
+        Move(_lastAppliedForce, _boostSpeed);
+    }
+
+    /// <summary>
+    /// Applies force to the RigidBody.
+    /// </summary>
+    /// <param name="force">The direction of force.</param>
+    /// <param name="speed">The speed multiplier of the force.</param>
+    private void Move(Vector2 force, float speed)
+    {
+        _body.AddForce(new Vector2(force.x * speed, force.y * speed));
+        _lastAppliedForce = force;
     }
 }
